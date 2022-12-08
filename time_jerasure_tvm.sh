dcb456a8c39913f9233024d03c1f777578dcec89
JiyuuuHuuu <hu_jiyu@icloud.com>
2022-12-08 02:34:51 -0700

    add timing script

--- time_jerasure_tvm.sh
+++ time_jerasure_tvm.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-for i in {1..1000}; do
-    ./Examples/jerasure_tvm 10 4 8 128000 123 2 >> jerasure_tvm_result.txt
-done
+if [ $1 -eq 0 ]; then
+    for i in {1..1000}; do
+        ./Examples/jerasure_tvm 10 4 8 128000 123 2 >> jerasure_tvm_result_0.txt
+    done
+elif [ $1 -eq 1 ]; then
+    for i in {1..1000}; do
+        ./Examples/time_tvm_decode 10 4 8 128000 123 >> jerasure_tvm_result_1.txt
+    done
+else
+    for i in {1..1000}; do
+        ./Examples/time_tvm_multiply 10 4 8 128000 123 >> jerasure_tvm_result_2.txt
+    done
+fi
